45b10fd726e248bc582ec0a5711caedc4387f5bc
p-acid <qkrtkstjd01@gmail.com>
2022-11-28 00:20:22 +0900

    chore(config): Revise blog meta config

--- lib/config/blog.ts
+++ lib/config/blog.ts
@@ -2,7 +2,7 @@
  * An object containing basic information in the blog.
  */
 export const META_CONFIG = {
-  title: 'Dev.log',
-  blog: 'https://github.com/p-acid/blog-template', // deployed blog link
-  github: 'https://github.com/p-acid/blog-template' // github url
+  title: 'Acid.log',
+  blog: 'https://www.acidlog.today/', // deployed blog link
+  github: 'https://github.com/p-acid/acidlog' // github url
 }
