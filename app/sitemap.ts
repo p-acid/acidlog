ced5dae26c17c82bcfe37568b7a86cdb863efb87
p-acid <qkrtkstjd01@gmail.com>
2024-07-29 16:20:36 +0900

    chore: update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,48 +3,53 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.acidlog.life/",
-      lastModified: "2024-05-08T06:04:16+00:00",
+      url: "https://acidlog.world/",
+      lastModified: "2024-07-29T07:17:33+00:00",
       priority: 1.0,
     },
     {
-      url: "https://www.acidlog.life/docs",
-      lastModified: "2024-05-08T06:04:16+00:00",
+      url: "https://acidlog.world/docs",
+      lastModified: "2024-07-29T07:17:33+00:00",
       priority: 0.8,
     },
     {
-      url: "https://www.acidlog.life/projects",
-      lastModified: "2024-05-08T06:04:16+00:00",
+      url: "https://acidlog.world/projects",
+      lastModified: "2024-07-29T07:17:33+00:00",
       priority: 0.8,
     },
     {
-      url: "https://www.acidlog.life/about",
-      lastModified: "2024-05-08T06:04:16+00:00",
+      url: "https://acidlog.world/about",
+      lastModified: "2024-07-29T07:17:33+00:00",
       priority: 0.8,
     },
     {
-      url: "https://www.acidlog.life/posts/handle-markdown-with-notion-api",
-      lastModified: "2024-05-08T06:04:16+00:00",
+      url: "https://acidlog.world/posts/static-contents-management-with-notion",
+      lastModified: "2024-07-29T07:17:33+00:00",
       priority: 0.8,
     },
     {
-      url: "https://www.acidlog.life/posts/switter-review",
-      lastModified: "2024-05-08T06:04:16+00:00",
+      url: "https://acidlog.world/posts/switter-review",
+      lastModified: "2024-07-29T07:17:33+00:00",
       priority: 0.8,
     },
     {
-      url: "https://www.acidlog.life/posts/2023-review",
-      lastModified: "2024-05-08T06:04:16+00:00",
+      url: "https://acidlog.world/posts/2023-review",
+      lastModified: "2024-07-29T07:17:33+00:00",
       priority: 0.8,
     },
     {
-      url: "https://www.acidlog.life/posts/why-use-git-flow",
-      lastModified: "2024-05-08T06:04:16+00:00",
+      url: "https://acidlog.world/posts/why-use-git-flow",
+      lastModified: "2024-07-29T07:17:33+00:00",
       priority: 0.8,
     },
     {
-      url: "https://www.acidlog.life/docs/google-ads-api-with-node",
-      lastModified: "2024-05-08T06:04:16+00:00",
+      url: "https://acidlog.world/docs/bundler-basics",
+      lastModified: "2024-07-29T07:17:33+00:00",
+      priority: 0.64,
+    },
+    {
+      url: "https://acidlog.world/docs/transpiler-basics",
+      lastModified: "2024-07-29T07:17:33+00:00",
       priority: 0.64,
     },
   ];
