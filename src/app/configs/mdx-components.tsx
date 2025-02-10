8445a05147675c1b887dc3c64feafb19b8c77015
Samuel Park <87939521+p-acid@users.noreply.github.com>
2025-02-10 19:48:19 +0900

    docs: frontend diving club review (#10)

--- src/app/configs/mdx-components.tsx
+++ src/app/configs/mdx-components.tsx
@@ -1,10 +1,19 @@
 import type { MDXComponents } from "mdx/types";
 
 import { Callout, Code, Video } from "@/shared/ui";
+import { ImgHTMLAttributes } from "react";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     a: (props) => <a target="_blank" {...props} />,
+    img: (props: ImgHTMLAttributes<HTMLImageElement>) => (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img
+        className="max-h-[50vh] w-full bg-slate-950 object-contain"
+        alt="이미지"
+        {...props}
+      />
+    ),
     Video,
     Code,
     Callout,
